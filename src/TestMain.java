562e86314a0c1de47a8f40c6ed98634b58279e4e
zkk97 <71272977+zkk97@users.noreply.github.com>
2020-10-03 20:24:06 +0800

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,7 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1111111111111111");
+		System.out.println("12");
 	}
 
 }
